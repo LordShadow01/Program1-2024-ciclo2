4c01425fd078922c11919257ebc0d291b259042f
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-25 17:03:10 -0600

    SUMA 2 NUMEROS DECIMALES

---
+++ SUMA DE 2 NUMEROS DECIMALES/SUMA DE 2 NUMEROS DECIMALES/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_DE_2_NUMEROS_DECIMALES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // suma de decimales de dos numeros
+
+            Console.Write("num 1:");
+            double num1 = double.Parse(Console.ReadLine()); //"5" ->
+
+            Console.Write("num 2:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}

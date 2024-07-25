1ec659b5514d03f32a8155ae70e0c30aec23718a
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-25 17:24:39 -0600

    SUMA CON SBYT

---
+++ SUMA DE 2 NUMEROS CON SBYT/SUMA DE 2 NUMEROS CON SBYT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_DE_2_NUMEROS_CON_SBYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // suma de dos numeros SBYT
+
+            Console.Write("num 1:");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Console.Write("num 2:");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
+
+
+            int respuesta = num1 + num2;
+
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}

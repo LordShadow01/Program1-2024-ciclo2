8a0f0b0c81263912640d6d85eedcf1b98246406c
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-25 17:07:39 -0600

    SUMA

---
+++ SUMA DE 2 NUMEROS/SUMA DE 2 NUMEROS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_DE_2_NUMEROS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // suma de dos numeros
+
+            Console.Write("num 1:");
+            int num1 = int.Parse(Console.ReadLine()); //"5" ->
+
+            Console.Write("num 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
